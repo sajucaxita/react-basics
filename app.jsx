846c43d299960007f0f251f7467a93ef40d2ced1
sajucaxita <sajucaxita@gmail.com>
2024-05-03 17:06:09 +0530

    day 2

--- app.jsx
+++ app.jsx
@@ -1,7 +1,7 @@
 
     import React from 'react'
     import ReactDOM from 'react-dom/client'
-    import NewComponent from './src/NewComponent'
+    import NewComponent from './app/src/NewComponent'
     
 
     const root = ReactDOM.createRoot(document.getElementById('root'))
