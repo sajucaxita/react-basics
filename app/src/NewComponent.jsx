06a2021dd450bf59c294fb7321ad1af5203d58f9
sajucaxita <sajucaxita@gmail.com>
2024-05-03 17:05:29 +0530

    day 2

---
+++ app/src/NewComponent.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+const NewComponent = ()=>{
+    const arr =[1,2,3]
+    return(
+        <div>
+            
+           
+        </div>
+    )
+}
+
+export default NewComponent
+
